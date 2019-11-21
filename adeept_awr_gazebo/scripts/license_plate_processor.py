4fd214320fbc0c7fdd498729fcd3c4e79b89a2b7
Richard Echegaray <richard0108@hotmail.com>
2019-11-20 16:34:55 -0800

    integrated stuff, made sure not to commit the pyc file

--- adeept_awr_gazebo/scripts/license_plate_processor.py
+++ adeept_awr_gazebo/scripts/license_plate_processor.py
@@ -9,6 +9,8 @@ from keras import layers
 from keras import models
 from keras import optimizers
 
+import tensorflow as tf
+
 from keras.utils import plot_model
 from keras import backend
 from matplotlib import pyplot as plt
@@ -23,11 +25,23 @@ from random import randint
 
 MIN_ASPECT_RATIO = 0.45
 
+config = tf.ConfigProto(
+    device_count={'GPU': 1},
+    intra_op_parallelism_threads=1,
+    allow_soft_placement=True
+)
+
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.6
+
 class license_plate_processor:
 
     def __init__(self):
         self.license_plate_image = None
         self.location_image = None
+        self.session = tf.Session(config=config)
+
+        keras.backend.set_session(self.session)
         self.license_plate_model = load_model('grayscale_less_blur.h5')
         self.license_plate_model._make_predict_function()
         #self.license_plate_pub = rospy.Publisher("/license_plate topic", std_msgs.msg.String, queue_size=30)
@@ -226,10 +240,12 @@ class license_plate_processor:
             if(index == 2):
                 plate_string = plate_string + "_"
             img_aug = np.expand_dims(character, axis=0)
-            y_predict = self.license_plate_model.predict(img_aug)[0]
-            order = [i for i, j in enumerate(y_predict) if j > 0.5]
-            print(order)
-            plate_string = plate_string + str(self.character_map[order[0]])
+            with self.session.as_default():
+                with self.session.graph.as_default():
+                    y_predict = self.license_plate_model.predict(img_aug)[0]
+                    order = [i for i, j in enumerate(y_predict) if j > 0.5]
+                    print(order)
+                    plate_string = plate_string + str(self.character_map[order[0]])
         
         return plate_string
     
