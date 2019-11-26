30f8ef3feef36d35233218b2252e6b15f2bd98f9
Richard Echegaray <richard0108@hotmail.com>
2019-11-26 02:20:18 -0800

    finished integrating inner loop with outer loop! now need to uncomment cnn stuff

--- adeept_awr_gazebo/scripts/drive.py
+++ adeept_awr_gazebo/scripts/drive.py
@@ -11,18 +11,29 @@ from license_plate_processor import license_plate_processor
 
 KP = 0.001
 KD = 0.0005
+
 NUM_PIXELS_X = 1280
 NUM_PIXELS_Y = 379
-READING_GAP = 215 #205
+READING_GAP = 215 
 Y_READING_LEVEL = 345
-Y_READ_PLATES = 230 #270
+Y_READ_PLATES = 230 
+X_READ_INNER_PLATES = 1000
 Y_READ_PED = 90 
+Y_CHECK_TRUCK = 70
+X_CHECK_TRUCK_START = 430
+X_CHECK_TRUCK_END = 600
+X_CHECK_INNER_BLUE = 950
+Y_CHECK_INNER_BLUE = 100
+Y_CHECK_OUTER_BLUE = 120
+
 
 ### STATES ### 
-INITIALIZE = -1
-DRIVING = 0
-WATCHING = 1
-CROSS_THE_WALK = 2
+INITIALIZE = 0
+DRIVING_INNER = 1
+TRANSITION = 2
+DRIVING = 3
+WATCHING = 4
+CROSS_THE_WALK = 5
 
 class state_machine:
 
@@ -33,10 +44,15 @@ class state_machine:
         self.bridge = CvBridge()
         self.last_err = 0
         self.last_pos = 0
-        self.current_state = INITIALIZE#INITIALIZE
+        self.current_state = INITIALIZE #INITIALIZE
         self.starting_time = time.time()
-        self.lpp = license_plate_processor()
-    
+        self.num_of_plates_snapped = 0
+        self.initalized = False
+        self.truck_spotted = False
+        self.first_inner_car = False
+        self.first_inner_pic_snapped = False
+        print("Let's start with the inner plates...")
+        #self.lpp = license_plate_processor()
         
    
     def callback(self,data):
@@ -46,16 +62,88 @@ class state_machine:
             print(e)
         frame = state_machine.image_converter(cv_image) # crops 
 
-        if self.current_state == INITIALIZE:
+        if self.current_state == INITIALIZE: #for turning into initial loop first
             time_elapsed = time.time() - self.starting_time
-            if time_elapsed < 0.5:
+            if time_elapsed < 3 and not self.initalized:
+                self.speed_controller(500)
+            else:
+                if not self.initalized:
+                    print("We're gonna let the truck pass first...")
+                self.initalized = True
+                if not self.truck_spotted:
+                    if self.watch_truck(frame):
+                        self.truck_spotted = True
+                        print("There it is! Now let's give it a head start...")
+                    # print("looking for truck: " + str(time_elapsed))
+                    self.stop()
+                    self.starting_time = time.time()
+                elif time_elapsed < 7:
+                    # print("waiting 7 sec: " + str(time_elapsed))
+                    self.stop()
+                elif time_elapsed < 7.5:
+                    # print("going forward a bit: " + str(time_elapsed))
+                    self.speed_controller(0)
+                elif time_elapsed < 8.1: #7.75
+                    # print("rotating a bit:" + str(time_elapsed))
+                    self.speed_controller(500)
+                else:
+                    # print("driving!")
+                    self.starting_time = time.time()
+                    self.current_state = DRIVING_INNER
+                
+        elif self.current_state == DRIVING_INNER:
+            time_elapsed = time.time() - self.starting_time
+      
+            position = state_machine.get_position(frame, self.last_pos)
+            self.last_pos = position
+            self.speed_controller(position)
+
+            if not self.first_inner_car:
+                if self.check_blue_car(frame, True) and not self.first_inner_pic_snapped:
+                    time_elapsed = 0
+                    self.stop()
+                    self.first_inner_pic_snapped = True
+                    self.starting_time = time.time()
+                    print("Inner License plate snapped")
+                    # print(time_elapsed)
+                    cv.imshow("Frame", frame)
+                
+                if time_elapsed < 6 and self.first_inner_pic_snapped:
+                    # print("waiting: " + str(time_elapsed))
+                    self.stop()
+                elif time_elapsed > 6 and self.first_inner_pic_snapped: 
+                    self.first_inner_car = True
+                    # print("onto second car")
+            else:
+                # print("second car")
+                if self.check_blue_car(frame, True):
+                    print("Inner License plate snapped")
+                    cv.imshow("Frame", frame)
+                    self.stop()
+                    self.current_state = TRANSITION    
+                    self.starting_time = time.time()
+        #         # img_msg = self.bridge.cv2_to_imgmsg(frame)
+        #         # self.image_pub.publish(img_msg)
+        #         #self.stop()
+        #         #cv.imwrite("./license_plates/" + str(randint(0,10000)) + ".png", frame)
+        #         #self.lpp.callback(frame)
+
+        elif self.current_state == TRANSITION:
+            time_elapsed = time.time() - self.starting_time
+            if time_elapsed < 1.5:
                 self.speed_controller(0)
-            elif time_elapsed < 0.75:
+            elif time_elapsed < 3:
+                self.speed_controller(500)
+            elif time_elapsed < 5.5:
+                self.speed_controller(0)
+            elif time_elapsed < 6:
                 self.speed_controller(500)
             else:
                 self.current_state = DRIVING
+                print("Onto the outer plates!")
+
         
-        if self.current_state == DRIVING:
+        elif self.current_state == DRIVING:
             position = state_machine.get_position(frame, self.last_pos)
             self.last_pos = position
             self.speed_controller(position)
@@ -65,20 +153,18 @@ class state_machine:
                 self.stop()
                 self.current_state = WATCHING
                 print("Stop! Looking for pedestrians...")
-            if self.check_blue_car(frame) and time_elapsed > 0.5:
+            if self.check_blue_car(frame, False) and time_elapsed > 0.5:
                 self.starting_time = time.time()
-                print("License plate snapped")
+                print("Outer License plate snapped")
                 cv.imshow("Frame", frame)
                 # img_msg = self.bridge.cv2_to_imgmsg(frame)
                 # self.image_pub.publish(img_msg)
-                self.stop()
-                cv.imwrite("./license_plates/" + str(randint(0,10000)) + ".png", frame)
-                self.lpp.callback(frame)
                 #self.stop()
-                #time.sleep(1)
+                #cv.imwrite("./license_plates/" + str(randint(0,10000)) + ".png", frame)
+                #self.lpp.callback(frame)
         
         elif self.current_state == WATCHING:
-            if self.watch(frame):
+            if self.watch_people(frame):
                 self.current_state = CROSS_THE_WALK
                 self.starting_time = time.time()
         
@@ -100,16 +186,21 @@ class state_machine:
 
         
         #      DEBUGGING TOOLS TO SEE BLACK AND WHITE
-        # cv.circle(frame, (READING_GAP, Y_READING_LEVEL), 15, (255,205,195), -1) #top two cicles are for linefollowing
-        # cv.circle(frame, (NUM_PIXELS_X-READING_GAP,Y_READING_LEVEL), 15, (255,205,195), -1)        
+        cv.circle(frame, (READING_GAP, Y_READING_LEVEL), 15, (255,205,195), -1) #top two cicles are for linefollowing
+        cv.circle(frame, (NUM_PIXELS_X-READING_GAP,Y_READING_LEVEL), 15, (255,205,195), -1)        
         # cv.circle(frame, (READING_GAP,Y_READ_PLATES), 15, (255,205,195), -1)  #reading for white here  
-        cv.circle(frame, (NUM_PIXELS_X/3+10,Y_READ_PED), 15, (255,205,195), -1) #checking for ped on left
+        # cv.circle(frame, (NUM_PIXELS_X/3+10,Y_READ_PED), 15, (255,205,195), -1) #checking for ped on left
         # cv.circle(frame, (2*NUM_PIXELS_X/3,Y_READ_PED), 15, (255,205,195), -1)
-    
-        # light_test = (20, 20, 20)
-        # dark_test = (110, 70, 70)
+        cv.circle(frame, (X_CHECK_TRUCK_START, Y_CHECK_TRUCK), 15, (255,205,195), -1) #top two cicles are for linefollowing
+        cv.circle(frame, (X_CHECK_TRUCK_END, Y_CHECK_TRUCK), 15, (255,205,195), -1) #top two cicles are for linefollowing
+        cv.circle(frame, (X_READ_INNER_PLATES, Y_READ_PLATES), 15, (255,205,195), -1)  #inner reading plate
+        cv.circle(frame, (NUM_PIXELS_X-1, Y_READ_PLATES), 15, (255,205,195), -1) #inner reading plate
+        cv.circle(frame, (X_CHECK_INNER_BLUE, Y_CHECK_INNER_BLUE), 15, (255,205,195), -1) #inner check blue
+
+        # light_test = (100, 100, 100)
+        # dark_test = (130, 130, 130)
         # frame = cv.inRange(frame, light_test, dark_test) #road is white and majority of other stuff is black
-        cv.imshow("Robot", frame)
+        cv.imshow("Robot's view :3", frame)
         cv.waitKey(3) 
 
 
@@ -179,7 +270,7 @@ class state_machine:
     #     elif check_frame[NUM_PIXELS_Y-1,NUM_PIXELS_X/3] == 0:
 
     @staticmethod
-    def check_blue_car(frame):
+    def check_blue_car(frame, inner_ring):
         light_blue = (85, 0, 0) # BGR 
         light_blue2 = (190,90,90)
         dark_blue = (135, 35, 35)
@@ -193,38 +284,54 @@ class state_machine:
         blue_mask = cv.inRange(frame, light_blue, dark_blue) #only red appears, all else black     
         blue2_mask = cv.inRange(frame, light_blue2, dark_blue2) #only red appears, all else black           
         white_mask = cv.inRange(frame, light_white, dark_white) #only red appears, all else black
-        
-        # inRange is x,y
-        for i in range(READING_GAP): #ReadGap
-             if (white_mask[Y_READ_PLATES,i] != 0):
-                 white_pixels = white_pixels + 1
+
+        if not inner_ring: 
+            # inRange is x,y
+            for i in range(READING_GAP): #ReadGap
+                if (white_mask[Y_READ_PLATES,i] != 0):
+                    white_pixels = white_pixels + 1
         #print("Num:" + " " +  str(white_pixels))
         
-        if (white_pixels < 50): #READING_GAP/4, 50
-            return 0
+            if (white_pixels < 50): #READING_GAP/4, 50
+                return 0
 
-        if ((blue_mask[150,0] != 0) or (blue2_mask[150,0] != 0)): #make sure edge isnt blue
-            return 0
+            if ((blue_mask[150,0] != 0) or (blue2_mask[150,0] != 0)): #make sure edge isnt blue
+                return 0
+            else:
+                #print("SAW WHITE")
+                check_stripes = 0
+                for i in range(2*READING_GAP+10): #ADDED THE +10, changed the y value from 150 to 60,
+                    if (((blue_mask[Y_CHECK_OUTER_BLUE,i] == 0) or (blue2_mask[Y_CHECK_OUTER_BLUE,i] == 0)) and check_stripes == 0): #first not blue
+                        check_stripes = 1
+                    if (((blue_mask[Y_CHECK_OUTER_BLUE,i] != 0) or (blue2_mask[Y_CHECK_OUTER_BLUE,i] != 0)) and check_stripes == 1): #first blue
+                        check_stripes = 2
+                    if (((blue_mask[Y_CHECK_OUTER_BLUE,i] == 0) or (blue2_mask[Y_CHECK_OUTER_BLUE,i] == 0)) and check_stripes == 2): #second not blue
+                        check_stripes = 3
+                    if (((blue_mask[Y_CHECK_OUTER_BLUE,i] != 0) or (blue2_mask[Y_CHECK_OUTER_BLUE,i] != 0)) and check_stripes == 3): #second blue
+                        check_stripes = 4
+                    if (check_stripes == 4):
+                        #print("passed stripes test")
+                        return 1
+                return 0
+        
+        elif inner_ring:
+            for i in range(NUM_PIXELS_X-X_READ_INNER_PLATES-1):
+                if (white_mask[Y_READ_PLATES,i+X_READ_INNER_PLATES] != 0):
+                    white_pixels = white_pixels + 1
+            
+            # print(white_pixels)
+            if (white_pixels < 80):
+                return 0           
 
-        else:
-            #print("SAW WHITE")
-            check_stripes = 0
-            for i in range(2*READING_GAP+10): #ADDED THE +10, changed the y value from 150 to 60,
-                if (((blue_mask[120,i] == 0) or (blue2_mask[120,i] == 0)) and check_stripes == 0): #first not blue
-                    check_stripes = 1
-                if (((blue_mask[120,i] != 0) or (blue2_mask[120,i] != 0)) and check_stripes == 1): #first blue
-                    check_stripes = 2
-                if (((blue_mask[120,i] == 0) or (blue2_mask[120,i] == 0)) and check_stripes == 2): #second not blue
-                    check_stripes = 3
-                if (((blue_mask[120,i] != 0) or (blue2_mask[120,i] != 0)) and check_stripes == 3): #second blue
-                    check_stripes = 4
-                if (check_stripes == 4):
-                    #print("passed stripes test")
+            for i in range(NUM_PIXELS_X-X_CHECK_INNER_BLUE-1):
+                if blue_mask[Y_CHECK_INNER_BLUE,i+X_CHECK_INNER_BLUE] != 0 or blue2_mask[Y_CHECK_INNER_BLUE,i+X_CHECK_INNER_BLUE] != 0: #first blue
+                    #print("FOUND INNER PLATE AYY")
                     return 1
+
             return 0
 
 
-    def watch(self, frame):  #y pixel at 85 is where we wanna look to see movement
+    def watch_people(self, frame):  #y pixel at 85 is where we wanna look to see movement
         #Y_READ_PED = 90             (NUM_PIXELS_X/3,Y_READ_PED)
         light_jeans = (20, 20, 20)
         dark_jeans = (110, 70, 70)
@@ -237,6 +344,23 @@ class state_machine:
 
         if jean_pixels > 10:
             print("hes on the left!!! go")
+            return 1
+        else:
+            return 0
+
+    def watch_truck(self, frame):  #gets like over 40 pixels pretty often
+        light_truck = (100, 100, 100)
+        dark_truck = (130, 130, 130)
+        truck_mask = cv.inRange(frame, light_truck, dark_truck) #road is white and majority of other stuff is black
+        truck_pixels = 0
+
+        for i in range(X_CHECK_TRUCK_END-X_CHECK_TRUCK_START): #   #430,70 to 600,70
+            if truck_mask[Y_CHECK_TRUCK, X_CHECK_TRUCK_START+i] != 0:
+                truck_pixels = truck_pixels + 1
+
+        # print("Num of truck pixels = " + str(truck_pixels))
+        if truck_pixels > 40:
+            # print("Truck spotted!")
             return 1
         else:
             return 0
